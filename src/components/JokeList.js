b2bc335daeb000746e5f13350ffdc7472aaed40f
Ruth Sargent <ruthsargent@ruths-mbp.home>
2020-02-28 15:22:23 +0000

    implemented infinite scroll

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -10,32 +10,50 @@ class JokeList extends Component {
   onHomeClick = () => {
     this.props.handleHomeClick();
   };
-  onMoreClick = () => {
-    this.setState({ isLoading: true }, () => {
-      api.getJokeList().then(jokes => {
-        this.setState({
-          jokes: [...this.state.jokes, ...jokes],
-          isLoading: false
-        });
-      });
-    });
-  };
+  // onMoreClick = () => {
+  //   this.setState({ isLoading: true }, () => {
+  //     api.getJokeList().then(jokes => {
+  //       this.setState({
+  //         jokes: [...this.state.jokes, ...jokes],
+  //         isLoading: false
+  //       });
+  //     });
+  //   });
+  // };
   componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
     api.getJokeList().then(jokes => {
-      console.log(jokes);
       this.setState({ jokes: jokes });
     });
   }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  handleScroll = event => {
+    const element = event.target.documentElement;
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      this.setState({ isLoading: true }, () => {
+        api.getJokeList().then(jokes => {
+          this.setState({
+            jokes: [...this.state.jokes, ...jokes],
+            isLoading: false
+          });
+        });
+      });
+    }
+  };
   render() {
     const { jokes, isLoading } = this.state;
     return (
-      <div className="Jokes">
+      <div className="Jokes" onScroll={this.handleScroll}>
+        <div className="buttons">
+          <button onClick={this.onHomeClick}>Home</button>
+        </div>
         {jokes.map(joke => {
           return <p key={joke.id}>- {joke.joke}</p>;
         })}
         {isLoading && <div>Loading...</div>}
-        <div>
-          <button onClick={this.onMoreClick}>More</button>
+        <div className="buttons">
           <button onClick={this.onHomeClick}>Home</button>
         </div>
       </div>
